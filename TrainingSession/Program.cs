77f77be1278c6236cb4958cf2a339301372ff14d
dipikak34 <dipikak34@users.noreply.github.com>
2023-06-10 19:53:06 +0530

    Add project files.

---
+++ TrainingSession/Program.cs
@@ -0,0 +1,299 @@
+﻿#region Sum of Two numbers
+
+//int a;
+//int b;
+
+//int c;
+
+//Console.WriteLine("Enter First Number");
+
+//a = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter Second Number");
+
+//b = Convert.ToInt32(Console.ReadLine());
+
+//c = a + b;
+
+//Console.WriteLine("Sum of Two Numbers is " + c);
+
+//Console.ReadLine();
+
+#endregion
+//..............................................................................................................................
+#region Substraction of Two numbers
+
+//int num1;
+//int num2;
+
+//int Result;
+
+//Console.WriteLine("Enter First Number");
+
+//num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter Second Number");
+
+//num2 = Convert.ToInt32(Console.ReadLine());
+
+//Result = num1 - num2;
+
+//Console.WriteLine("Substraction of Two Number is " + Result);
+
+//Console.ReadLine();
+#endregion
+//..............................................................................................................................
+#region Multiplication of two numbers
+
+//int a = 10;
+//int b = 5;
+
+//int Result;
+
+//Result = a * b;
+
+//Console.WriteLine("Multiplication of two numbers is " + Result);
+
+//Console.ReadLine();
+#endregion
+//..............................................................................................................................
+
+#region Division of two numbers
+
+//int a;
+//int b;
+
+//int c;
+
+//Console.WriteLine("Enter First Number");
+
+//a = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter Second Number");
+
+//b = Convert.ToInt32(Console.ReadLine());
+
+//c = a / b;
+
+//Console.WriteLine("Division of two number is " + c);
+//Console.ReadLine();
+#endregion
+//..............................................................................................................................
+
+#region Largest of two number using if statement
+
+//int a;
+//int b;
+
+//Console.WriteLine("Write any number");
+
+
+//a =Convert.ToInt32(Console.ReadLine());
+//b = Convert.ToInt32(Console.ReadLine());
+
+//if (a >= b)
+//{
+//    Console.WriteLine("{0} is Largest number", +a);
+//}
+//else
+//{
+//    Console.WriteLine("{0} is Largest number", +b);
+//}
+//Console.ReadLine();
+#endregion
+//..............................................................................................................................
+
+#region Largest between Three Numbers
+
+//int num1;
+//int num2;
+//int num3;
+
+//Console.WriteLine("Write any number");
+
+//num1=Convert.ToInt32(Console.ReadLine());
+
+//num2=Convert.ToInt32(Console.ReadLine());
+
+//num3=Convert.ToInt32(Console.ReadLine());
+
+//if(num1 > num2)
+//{
+//    if (num1 > num3)
+//    {
+//        Console.WriteLine("largest No between three numbers is " + num1);
+//    }
+//}
+//else if(num2 > num3)
+//{
+//    Console.WriteLine("Largest number between three no. is " + num2);
+//}
+//else
+//{
+//    Console.WriteLine("Largest number between three no is " + num3);
+//}
+
+#endregion
+//..............................................................................................................................
+
+#region Find Even number
+
+//int a;
+
+//Console.WriteLine("Enter a Number");
+//a = Convert.ToInt32(Console.ReadLine());
+
+//if (a % 2 == 0)
+//{
+//    Console.WriteLine("This is Even Number");
+//}
+//else
+//{
+//    Console.WriteLine("This is Odd Number");
+//}
+#endregion
+//..............................................................................................................................
+
+#region Find the no is Divided by 5 and 10
+
+//int i;
+
+//Console.WriteLine(" Enter a Number");
+
+//i=Convert.ToInt32(Console.ReadLine());
+
+//if(i % 5==0 && i %10 ==0)
+//{
+//    Console.WriteLine("This no is Divisible by 5 and 10");
+//}
+//else
+//{
+//    Console.WriteLine("This no is not Divisible by 5 and 10");
+//}
+
+#endregion
+//..............................................................................................................................
+
+#region if sum of two number is greater then 100 or not
+
+//int a;
+//int b;
+
+//int Sum;
+
+//Console.WriteLine("Enter First Number");
+
+//a=Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second Number");
+
+//b=Convert.ToInt32(Console.ReadLine());
+
+//Sum = a + b;
+
+//if(Sum>=100)
+//{
+//    Console.WriteLine("Sum of two Number is Greater than 100" );
+//}
+//else
+//{
+//    Console.WriteLine("Sum of two Number is not Greater than 100");
+//}
+//Console.ReadLine();
+#endregion
+//.............................................................................................................................
+
+#region if multiplication of two number is greater than 200
+
+//int num1;
+//int num2;
+
+//int Mul;
+
+//Console.WriteLine("Enter First Number");
+
+//num1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Enter second Number");
+
+//num2= Convert.ToInt32(Console.ReadLine());
+
+//Mul = num1 * num2;
+
+//if (Mul >= 200)
+//{
+//    Console.WriteLine("Multiplication of two Number is Greater than 200");
+//}
+//else
+//{
+//    Console.WriteLine("Multiplication of two Number is not Greater than 200");
+//}
+//Console.ReadLine();
+#endregion
+//..............................................................................................................................
+
+#region The example of modulas operator
+
+//int a = 11;
+//int b=5;
+
+//int Reminder = a % b;
+
+//Console.WriteLine("The Reminder will be " + Reminder);
+
+
+#endregion
+//..............................................................................................................................
+//Qutient
+
+
+//int a = 10;
+//int b = 5;
+
+//int Qutient = a % b;
+
+//Console.WriteLine("The Qutient will be " + Qutient);
+//.................................................................................................................................
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
